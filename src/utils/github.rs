c36125949a96fbf1f58bbafef98a074d4739ebad
strbrgr <jocsti@web.de>
2025-06-21 18:06:57 -0500

    refactor github utils again

--- src/utils/github.rs
+++ src/utils/github.rs
@@ -1,61 +1,39 @@
-use reqwest::Client;
+use ratatui::widgets::ListState;
+use reqwest::{Client, StatusCode};
 use serde_json::Value;
 
-use crate::components::list::{RepoItem, Status};
+use crate::components::list::{RepoItem, Repositories, RepositoryInfo, Status};
 
-// What we are fetching
-enum GithubResource {
-    UserName,
-    RepoNames,
+pub async fn get_data_from_github(token: &str) -> Result<Repositories, Box<dyn std::error::Error>> {
+    let owner = get_user(token).await?;
+    let repo_items = get_repos(&owner, token).await?;
+
+    Ok(Repositories { owner, repo_items })
 }
 
-enum GithubResponse {
-    UserNames(Vec<String>),
-    RepoItems(Vec<RepoItem>),
-}
-
-fn extract_username(response: &GithubResponse) -> Result<&String, Box<dyn std::error::Error>> {
-    if let GithubResponse::UserNames(usernames) = response {
-        usernames.first().ok_or_else(|| "No username found".into())
-    } else {
-        Err("Expected UserNames variant".into())
-    }
-}
-
-pub async fn get_repos_from_github(
+pub async fn delete_repo(
+    owner: &str,
+    repo: &str,
     token: &str,
-) -> Result<Vec<RepoItem>, Box<dyn std::error::Error>> {
-    let user_response = get_user(token).await?;
-    let user_login = extract_username(&user_response)?;
-    let repos_response = get_repos(user_login, token).await?;
+) -> Result<StatusCode, Box<dyn std::error::Error>> {
+    let delete_uri = format!("https://api.github.com/repos/{owner}/{repo}");
 
-    if let GithubResponse::RepoItems(items) = repos_response {
-        Ok(items)
-    } else {
-        Err("Expected RepoItems variant".into())
-    }
-}
-
-async fn get_user(token: &str) -> Result<GithubResponse, Box<dyn std::error::Error>> {
-    let user_uri = "https://api.github.com/user";
-    get_resource(user_uri, GithubResource::UserName, token).await
-}
-
-async fn get_repos(
-    user_login: &str,
-    token: &str,
-) -> Result<GithubResponse, Box<dyn std::error::Error>> {
-    let repos_uri = format!("https://api.github.com/users/{user_login}/repos");
-    get_resource(&repos_uri, GithubResource::RepoNames, token).await
-}
-
-async fn get_resource(
-    uri: &str,
-    resource: GithubResource,
-    token: &str,
-) -> Result<GithubResponse, Box<dyn std::error::Error>> {
     let res = Client::new()
-        .get(uri)
+        .delete(delete_uri)
+        .header("Accept", "application/vnd.github+json")
+        .header("Authorization", format!("Bearer {token}"))
+        .header("X-GitHub-Api-Version", "2022-11-28")
+        .header("User-Agent", "knife")
+        .send()
+        .await?;
+
+    Ok(res.status())
+}
+
+pub async fn get_user(token: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let user_uri = "https://api.github.com/user";
+    let res = Client::new()
+        .get(user_uri)
         .header("Accept", "application/vnd.github+json")
         .header("Authorization", format!("Bearer {token}"))
         .header("X-GitHub-Api-Version", "2022-11-28")
@@ -66,30 +44,44 @@ async fn get_resource(
     let body = res.text().await?;
 
     let value: Value = serde_json::from_str(&body)?;
-    match resource {
-        GithubResource::UserName => {
-            let primary_value = value
-                .get("login")
-                .and_then(Value::as_str)
-                .ok_or("Primary key not found")?;
-            Ok(GithubResponse::UserNames(vec![primary_value.to_string()]))
-        }
-        GithubResource::RepoNames => {
-            let names = value
-                .as_array()
-                .ok_or("Expected an array")?
-                .iter()
-                .filter_map(|item| item.get("name").and_then(Value::as_str))
-                .map(|s| RepoItem {
-                    repo: s.to_owned(),
-                    status: Status::Unselected,
-                })
-                .collect::<Vec<_>>();
-            Ok(GithubResponse::RepoItems(names))
-        }
-    }
+    let primary_value = value
+        .get("login")
+        .and_then(Value::as_str)
+        .ok_or("Primary key not found")?;
+
+    Ok(primary_value.to_owned())
 }
 
-async fn delete_repos() {
-    todo!()
+pub async fn get_repos(
+    user_login: &str,
+    token: &str,
+) -> Result<RepositoryInfo, Box<dyn std::error::Error>> {
+    let repos_uri = format!("https://api.github.com/users/{user_login}/repos");
+    let res = Client::new()
+        .get(repos_uri)
+        .header("Accept", "application/vnd.github+json")
+        .header("Authorization", format!("Bearer {token}"))
+        .header("X-GitHub-Api-Version", "2022-11-28")
+        .header("User-Agent", "knife")
+        .send()
+        .await?;
+
+    let body = res.text().await?;
+
+    let value: Value = serde_json::from_str(&body)?;
+
+    let repos = value
+        .as_array()
+        .ok_or("Expected an array")?
+        .iter()
+        .filter_map(|item| item.get("name").and_then(Value::as_str))
+        .map(|s| RepoItem {
+            name: s.to_owned(),
+            status: Status::Unselected,
+        })
+        .collect::<Vec<_>>();
+    Ok(RepositoryInfo {
+        repos,
+        list_state: ListState::default(),
+    })
 }
