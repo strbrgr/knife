dc8acb7ab9ef996b2fa8d25526702ea6b5bce986
strbrgr <jocsti@web.de>
2025-06-21 19:01:48 -0500

    provide helper methods so that we can see a cursor on input

--- src/app.rs
+++ src/app.rs
@@ -9,6 +9,10 @@ pub enum Mode {
 
 pub struct App {
     pub exit: bool,
+    /// Current value of the input box
+    input: String,
+    /// Position of cursor in the editor area.
+    pub character_index: usize,
     pub token: String,
     pub token_input: String,
     pub waiting_for_token: bool,
@@ -21,6 +25,8 @@ impl App {
     pub fn new() -> App {
         App {
             exit: false,
+            input: String::new(),
+            character_index: 0,
             token: String::new(),
             token_input: String::new(),
             waiting_for_token: false,
@@ -32,6 +38,69 @@ impl App {
 
     pub fn exit(&mut self) {
         self.exit = true;
+    }
+
+    pub fn move_cursor_left(&mut self) {
+        let cursor_moved_left = self.character_index.saturating_sub(1);
+        self.character_index = self.clamp_cursor(cursor_moved_left);
+    }
+
+    pub fn move_cursor_right(&mut self) {
+        let cursor_moved_right = self.character_index.saturating_add(1);
+        self.character_index = self.clamp_cursor(cursor_moved_right);
+    }
+
+    pub fn enter_char(&mut self, new_char: char) {
+        let index = self.byte_index();
+        self.token_input.insert(index, new_char);
+        self.move_cursor_right();
+    }
+
+    /// Returns the byte index based on the character position.
+    ///
+    /// Since each character in a string can be contain multiple bytes, it's necessary to calculate
+    /// the byte index based on the index of the character.
+    fn byte_index(&self) -> usize {
+        self.token_input
+            .char_indices()
+            .map(|(i, _)| i)
+            .nth(self.character_index)
+            .unwrap_or(self.token_input.len())
+    }
+
+    pub fn delete_char(&mut self) {
+        let is_not_cursor_leftmost = self.character_index != 0;
+        if is_not_cursor_leftmost {
+            // Method "remove" is not used on the saved text for deleting the selected char.
+            // Reason: Using remove on String works on bytes instead of the chars.
+            // Using remove would require special care because of char boundaries.
+
+            let current_index = self.character_index;
+            let from_left_to_current_index = current_index - 1;
+
+            // Getting all characters before the selected character.
+            let before_char_to_delete = self.token_input.chars().take(from_left_to_current_index);
+            // Getting all characters after selected character.
+            let after_char_to_delete = self.token_input.chars().skip(current_index);
+
+            // Put all characters together except the selected one.
+            // By leaving the selected one out, it is forgotten and therefore deleted.
+            self.token_input = before_char_to_delete.chain(after_char_to_delete).collect();
+            self.move_cursor_left();
+        }
+    }
+
+    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
+        new_cursor_pos.clamp(0, self.token_input.chars().count())
+    }
+
+    pub fn reset_cursor(&mut self) {
+        self.character_index = 0;
+    }
+
+    pub fn submit_message(&mut self) {
+        self.token_input.clear();
+        self.reset_cursor();
     }
 
     pub fn select_next(&mut self) {
