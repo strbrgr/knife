dd57a625a9c69c4f87480a78692b66cc53dfe8f8
strbrgr <jocsti@web.de>
2025-06-26 07:11:38 -0500

    update uri naming

--- src/utils/github.rs
+++ src/utils/github.rs
@@ -5,7 +5,7 @@ use serde_json::Value;
 use crate::components::list::{RepoItem, Repositories, RepositoryInfo, Status};
 
 pub async fn get_data_from_github(token: &str) -> Result<Repositories, Box<dyn std::error::Error>> {
-    let owner = get_user(token).await?;
+    let owner = get_owner(token).await?;
     let repo_items = get_repos(&owner, token).await?;
 
     Ok(Repositories { owner, repo_items })
@@ -16,10 +16,10 @@ pub async fn delete_repo(
     repo: &str,
     token: &str,
 ) -> Result<StatusCode, Box<dyn std::error::Error>> {
-    let delete_uri = format!("https://api.github.com/repos/{owner}/{repo}");
+    let uri = format!("https://api.github.com/repos/{owner}/{repo}");
 
     let res = Client::new()
-        .delete(delete_uri)
+        .delete(uri)
         .header("Accept", "application/vnd.github+json")
         .header("Authorization", format!("Bearer {token}"))
         .header("X-GitHub-Api-Version", "2022-11-28")
@@ -30,10 +30,10 @@ pub async fn delete_repo(
     Ok(res.status())
 }
 
-pub async fn get_user(token: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let user_uri = "https://api.github.com/user";
+pub async fn get_owner(token: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let uri = "https://api.github.com/user";
     let res = Client::new()
-        .get(user_uri)
+        .get(uri)
         .header("Accept", "application/vnd.github+json")
         .header("Authorization", format!("Bearer {token}"))
         .header("X-GitHub-Api-Version", "2022-11-28")
@@ -48,17 +48,16 @@ pub async fn get_user(token: &str) -> Result<String, Box<dyn std::error::Error>>
         .get("login")
         .and_then(Value::as_str)
         .ok_or("Primary key not found")?;
-
     Ok(primary_value.to_owned())
 }
 
 pub async fn get_repos(
-    user_login: &str,
+    owner: &str,
     token: &str,
 ) -> Result<RepositoryInfo, Box<dyn std::error::Error>> {
-    let repos_uri = format!("https://api.github.com/users/{user_login}/repos");
+    let uri = format!("https://api.github.com/users/{owner}/repos");
     let res = Client::new()
-        .get(repos_uri)
+        .get(uri)
         .header("Accept", "application/vnd.github+json")
         .header("Authorization", format!("Bearer {token}"))
         .header("X-GitHub-Api-Version", "2022-11-28")
